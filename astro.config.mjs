5b863aab2e67681bfe80af8fb3a2886ce86bf054
かなた <188720002+pen-2@users.noreply.github.com>
2026-09-02 23:39:18 +0900

    rebrand: サイト名を稜線ノートからやまかなたに変更し、ユーザーページ形式のURLに移行

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://pen-2.github.io',
-	base: '/ridgeline-note/',
+	site: 'https://kanata3180.github.io',
 });
